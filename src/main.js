e13fee622fff6dbe29d6b51d8c3e2a9b67b0ca87
zxfjd3g <258147149@qq.com>
2020-05-04 12:00:25 +0800

    router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
+/* 
+入口js
+*/
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import App from '@/App'
+import router from './router'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false // 去掉不是生产环境的提示
 
 new Vue({
-  render: h => h(App),
+  // el: '#app'
+  render: h => h(App), // 将App组件的对象界面渲染到页面上
+  router, // 配置路由器
 }).$mount('#app')
+
+// var a = 3
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+/* 
+路由器对象模块
+*/
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import routes from './routes'
+
+
+// 声明使用vue插件
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  mode: 'history', // 不带#的模式
+  routes, // 配置所有路由
+})
+
+
