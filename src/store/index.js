8c89e750bc7db0b06eaf2fd79f59831f0e1bf559
zxfjd3g <258147149@qq.com>
2020-05-05 17:00:34 +0800

    总结

--- src/store/index.js
+++ src/store/index.js
@@ -3,9 +3,7 @@ vuex最核心的管理对象: store
 */
 import Vue from 'vue'
 import Vuex from 'vuex'
-
-import home from './modules/home'
-import user from './modules/user'
+import modules from './modules'
 
 // 声明使用vuex的插件
 Vue.use(Vuex)
@@ -26,28 +24,9 @@ const getters = {
 
 // 向外暴露store对象
 export default new Vuex.Store({
-  // state,
   mutations,
   actions,
   getters,
-  modules: {// 注册vuex的多个子模块
-    home,
-    user
-  }
+  modules
 })
 
-/* 
-vuex的store管理的总state是什么结构:
-  {
-    home: {
-      baseCategoryList: [],
-      x: {},
-      y: ''
-    },
-
-    user: {
-      userInfo: {},
-      x: ''
-    }
-  }
-*/
