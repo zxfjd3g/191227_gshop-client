71c7564aa5e08746cf44ad1e3ff9e9ceaecb4fa5
zxfjd3g <258147149@qq.com>
2020-05-05 11:13:56 +0800

    ajax

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -3,18 +3,31 @@
     <TypeNav/>
     <ListContainer/>
     <TodayRecommend/>
+    <Rank/>
+    <Like/>
+    <Floor/>
+    <Floor/>
+    <Brand/>
   </div>
 </template>
 
 <script>
 import ListContainer from './ListContainer/ListContainer'
 import TodayRecommend from './TodayRecommend/TodayRecommend'
+import Rank from './Rank/Rank'
+import Like from './Like/Like'
+import Floor from './Floor/Floor'
+import Brand from './Brand/Brand'
 export default {
   name: 'Home',
 
   components: {
     ListContainer,
-    TodayRecommend
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor,
+    Brand
   }
 }
 </script>
